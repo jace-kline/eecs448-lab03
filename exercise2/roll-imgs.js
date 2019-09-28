8ce4ac4878ce56378fc5a402b8182e4ffdb75a14
jace-kline <jace_kline@ku.edu>
2019-09-28 09:44:59 -0500

    Completed exercise 2.

--- exercise2/roll-imgs.js
+++ exercise2/roll-imgs.js
@@ -1,14 +1,26 @@
-
-let srcLoc = "img/"
+let curImgCode = 0
+let srcLoc = "./img/"
 let codeImgMap = {
-    0: (srcLoc + "mandelbrot.jpg"),
-    1: (srcLoc + "juliaset.jpg")
+    0: (srcLoc + "mandelbot.jpg"),
+    1: (srcLoc + "juliaset.jpg"),
+    2: (srcLoc + "serpinski.png"),
+    3: (srcLoc + "hilbertscurve.png"),
+    4: (srcLoc + "tree.jpg")
 }
 
 function nextImg() {
-    curImg
+    curImgCode = mod(curImgCode + 1, 5)
+    document.getElementById("im").src = codeImgMap[curImgCode]
 }
 
-function next(str) {
+function prevImg() {
+    curImgCode = mod(curImgCode - 1, 5)
+    document.getElementById("im").src = codeImgMap[curImgCode]
+}
 
+function mod(num, n) {
+    if(num < 0) {
+        return((num % n) + n)
+    }
+    return(num % n)
 }
